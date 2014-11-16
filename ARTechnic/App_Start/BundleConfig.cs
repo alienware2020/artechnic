747a67773b703aa587671a746c1612be22ced7b4
alienware2020 <rajarajesh89@gmail.com>
2014-11-16 11:58:48 +0530

    bundling and minification

---
+++ ARTechnic/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ARTechnic.App_Start
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-1.11.1.min.js",
+                "~/Scripts/site-master.js").IncludeDirectory("~/Content/js", "*.js", true));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            //            "~/Scripts/jquery-ui-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.unobtrusive*",
+            //            "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/styles").Include("~/Content/style.css",
+                                                                 "~/Content/css/bootstrap.min.css",
+                                                                 "~/Content/fancybox/source/jquery.fancybox1c51.css",
+                                                                 "~/Content/css/hoverex-all.css",
+                                                                 "~/Content/css/vector-icons.css",
+                                                                 "~/Content/css/jquery.easy-pie-chart.css",
+                                                                 "~/Content/plugins/LayerSlider/css/layerslider.css",
+                                                                 "~/Content/plugins/revslider/rs-plugin/css/settings.css",
+                                                                 "~/Content/plugins/revslider/rs-plugin/css/captions.css"));
+           
+        }
+    }
+}
--- ARTechnic/Global.asax.cs
+++ ARTechnic/Global.asax.cs
@@ -3,9 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using ARTechnic;
+using ARTechnic.App_Start;
 
 namespace ARTechnic
 {
@@ -15,6 +17,8 @@ namespace ARTechnic
         {
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleTable.EnableOptimizations = true;
         }
 
         void Application_End(object sender, EventArgs e)
